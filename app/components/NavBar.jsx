144f7a3ef60341c3e8291aefef85976ff69e4dab
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2023-11-21 15:53:35 +0000

    servicedesk logo added

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,10 +1,19 @@
 import Link from 'next/link'
+import Logo from './ServicedeskLogo.png'
+import Image from 'next/image'
 
 import React from 'react'
 
 export default function NavBar() {
   return (
 <nav>
+    <Image
+    src={Logo}
+    alt='Helpdesk logo'
+    width={70}
+    quality={100}
+    placeholder='blur' 
+    />
 <h1>Helpdesk</h1>
 <Link href="/">Dashboard</Link>
 <Link href="/tickets">Tickets</Link>
