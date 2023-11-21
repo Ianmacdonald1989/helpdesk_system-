89862956a5726bc71f4fdbfbee0c288eae598df7
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2023-11-21 15:42:51 +0000

    tailwind Css and data added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
 import NavBar from './components/NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
         <NavBar />
         {children}
       </body>
