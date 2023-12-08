31c153e248637e37eceb743ec2598b8d283535df
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2023-12-08 15:15:51 +0000

    api route set up and tested in postman, not found page bug fixed

---
+++ app/(dashboard)/layout.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function DashboardLayout({children}) {
+return (
+<>
+<NavBar/>
+{children}
+</>
+)
+}
--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -2,6 +2,19 @@ import { notFound } from "next/navigation"
 
 export const dynamicParams = true
 
+export async function generateMetaData({params}) {
+
+    const id = params.id 
+    
+        const res = await fetch (`http://localhost:4000/tickets/${id}`)
+        const ticket = await res.json()
+
+    return {
+
+        title:`Helpdesk | ${ticket.title}`
+    }
+}
+
 export async function generateStaticParams(){
     const res =await fetch ('http://localhost:4000/tickets/')
 
