95472eac3bad4342c063dd4742a8583e6f3db24f
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2024-01-08 11:29:31 +0000

    supabase set up

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,20 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 export default function Login() {
+
+const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log('user login', email, password)
+}
+
 return (
     <main>
     <h2 className="text-center">Log in</h2>
+
+    <AuthForm handleSubmit={handleSubmit}/>
     </main>
 )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,21 @@
+"use client"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
+import AuthForm from "../AuthForm";
 
 export default function Signup() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.log('user signup', email, password)
+}
+
   return (
    <main>
     <h2 className="text-center">Sign up</h2>
+
+    <AuthForm handleSubmit={handleSubmit}/>
    </main>
   )
 }
