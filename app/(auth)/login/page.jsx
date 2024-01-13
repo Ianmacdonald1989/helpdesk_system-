dc2ec3cd7ae0c3c54effd41b56e0fccbb04f2f2b
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2024-01-13 13:57:18 +0000

    email verification issue, not recoreding login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,20 +1,21 @@
 "use client"
 
-import AuthForm from "../AuthForm";
+// components
+import AuthForm from "../verify/AuthForm"
 
 export default function Login() {
 
 const handleSubmit = async (e, email, password) => {
-    e.preventDefault()
+e.preventDefault()
 
-    console.log('user login', email, password)
+console.log(email, password)
 }
-
+[]
 return (
-    <main>
-    <h2 className="text-center">Log in</h2>
+<main>
+    <h2 className="text-center">Login</h2>
 
-    <AuthForm handleSubmit={handleSubmit}/>
-    </main>
+    <AuthForm handleSubmit={handleSubmit} />
+</main>
 )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,21 +1,44 @@
 "use client"
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 
-import AuthForm from "../AuthForm";
+// components
+import AuthForm from "../verify/AuthForm"
 
 export default function Signup() {
+  const router = useRouter()
+  const [error, setError] = useState('')
 
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
+    setError('')
 
-    console.log('user signup', email, password)
-}
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+    if (error) {
+      setError(error.message)
+    }
+    if (!error) {
+      router.push('/verify')
+    } 
+  }
 
   return (
-   <main>
-    <h2 className="text-center">Sign up</h2>
+    <main>
+      <h2 className="text-center">Sign up</h2>
 
-    <AuthForm handleSubmit={handleSubmit}/>
-   </main>
+      <AuthForm handleSubmit={handleSubmit} />
+
+      {error && (
+        <div className="error">{error}</div>
+      )}
+    </main>
   )
 }
